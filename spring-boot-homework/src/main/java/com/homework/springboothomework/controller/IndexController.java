ff10c5d7defcf3c07c708eb42c41a4c9c94a99fe
零落星涯 <781958167@qq.com>
2018-09-12 23:51:05 +0800

    course 5

---
+++ spring-boot-homework/src/main/java/com/homework/springboothomework/controller/IndexController.java
@@ -0,0 +1,17 @@
+package com.homework.springboothomework.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Slf4j
+@Controller
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        log.info("-------------------------------->");
+        return "index";
+    }
+
+}
